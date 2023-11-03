bfbb57303c33320f87fa5e163f67b78e1a147af5
thanh.nc <congthanh29897@gmail.com>
2023-11-04 00:20:16 +0700

    init project

---
+++ Services/Service.CouponAPI/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Service.CouponAPI.Models;
+
+namespace Service.CouponAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Coupon> Coupons { get; set; }
+    }
+}
